f00be235267a63923286faf4264b0a5540b3bcda
Ghazala Ahmed <ghazala599@gmail.com>
2023-01-31 13:28:08 -0800

    added to userroutes.js file

---
+++ routes/api/userroutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  updateUser,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUsers).post(createUser);
+
+// /api/uers/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+
+
+module.exports = router;
